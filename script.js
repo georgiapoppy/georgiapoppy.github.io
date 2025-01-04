a784c5c787b6a86e0cc30e1dc23c86e0d538da02
Georgia Mason <georgiapoppym@gmail.com>
2025-01-04 14:43:51 +1030

    changed photos + little updates

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
+    hideDiv("me1", "me2");
+}
+
+function hideDiv(a, b) {
+    const x = document.getElementById(a);
+    const y = document.getElementById(b);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+    } else {
+      x.style.display = "none";
+      y.style.display = "block";
+    }
 }
