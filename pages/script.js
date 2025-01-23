aca89380837d221bbbf4c3f62843792ce92edbed
Georgia Mason <georgiapoppym@gmail.com>
2025-01-23 15:34:36 +1030

    moved pages into folders + cloned wordle repo + added pygbag workflow action

---
+++ pages/script.js
@@ -0,0 +1,17 @@
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+    hideDiv("dark1", "dark2");
+    hideDiv("dark3", "dark4");
+}
+
+function hideDiv(a, b) {
+    const x = document.getElementById(a);
+    const y = document.getElementById(b);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+    } else {
+      x.style.display = "none";
+      y.style.display = "block";
+    }
+}
